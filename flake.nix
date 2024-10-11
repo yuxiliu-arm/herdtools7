ba1b31b407943d85c8b1f23431b7e3411cc9a511
crvdgc <ubikium@gmail.com>
2024-10-11 10:14:00 +0100

    DONT MERGE: add asllib/doc build environment

--- flake.nix
+++ flake.nix
@@ -7,15 +7,36 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine
+          {
+
+            inherit (pkgs.texlive)
+              scheme-medium
+              enumitem
+              adjustbox
+              mathpartir
+              comment
+              relsize
+              currfile
+              ;
+          };
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ gmp pkg-config graphviz ghostscript ] ++
+          buildInputs = with pkgs;
+            [
+              gmp
+              pkg-config
+              graphviz
+              ghostscript
+              tex
+            ] ++
             pkgs.lib.optionals pkgs.stdenv.isDarwin
               [ pkgs.darwin.apple_sdk.frameworks.CoreServices ];
-          shellHook = ''
-            PKG_CONFIG_PATH="${pkgs.gmp}:$PKG_CONFIG_PATH"
-          '';
+          shellHook =
+            ''
+              PKG_CONFIG_PATH="${pkgs.gmp}:$PKG_CONFIG_PATH"
+            '';
         };
       }
     );
