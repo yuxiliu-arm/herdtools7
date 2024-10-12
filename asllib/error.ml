f542403b4471fa193c9a6a46f6e951563d6d4260
crvdgc <ubikium@gmail.com>
2024-10-12 23:03:05 +0100

    [asl,test] Issue error instead of assertion failure
    Also update intersecting_slices.t

--- asllib/error.ml
+++ asllib/error.ml
@@ -33,6 +33,7 @@ type error_desc =
   | BadSlices of error_handling_time * slice list * int
   | BadSlice of slice
   | EmptySlice
+  | UnexpectedSliceArg of expr
   | TypeInferenceNeeded
   | UndefinedIdentifier of identifier
   | MismatchedReturnValue of string
@@ -160,6 +161,11 @@ module PPrint = struct
           (error_handling_time_to_string t)
           length pp_slice_list slices
     | BadSlice slice -> fprintf f "ASL error: invalid slice %a." pp_slice slice
+    | UnexpectedSliceArg e ->
+        fprintf f
+          "ASL error:@ Unexpected slice argument %a, but a slice is expected.@ \
+           Hint: slice a single bit at position x with [x:] instead of [x]."
+          pp_expr e
     | TypeInferenceNeeded ->
         pp_print_text f
           "ASL Internal error: Interpreter blocked. Type inference needed."
