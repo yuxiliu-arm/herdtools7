8c5069f5cc0fc7664cb5a02d00f8e94ed2f2284e
crvdgc <ubikium@gmail.com>
2024-11-16 21:49:39 +0000

    [test] Fix missing ASL file script
    git ls-tree doesn't seem to accept glob patterns, use ls-files instead.

--- asllib/tests/check-no-missing-file-in-run.sh
+++ asllib/tests/check-no-missing-file-in-run.sh
@@ -6,7 +6,7 @@ set -o nounset    # Exposes unset variables
 counter=0
 
 for d in "$1"/asllib/tests/*.t/; do
-  for f in $(git ls-tree --name-only HEAD "$d*.asl"); do
+  for f in $(git ls-files HEAD "$d*.asl"); do
     if ! grep -q "${f##*/}" "${d}run.t"; then
       echo "ASL file committed and not run: $f";
       counter=$counter+1;
