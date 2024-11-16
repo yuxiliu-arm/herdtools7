07537b81cb79be1c1389f8d55db43a48c9fdd67d
Hadrien Renaud <hadrien.renaud2@arm.com>
2024-11-16 15:55:04 +0000

    [tests] Check no forgotten files in 'run.t's

---
+++ asllib/tests/check-no-missing-file-in-run.sh
@@ -0,0 +1,20 @@
+set -o errexit    # Used to exit upon error, avoiding cascading errors
+set -o pipefail   # Unveils hidden failures
+set -o nounset    # Exposes unset variables
+
+counter=0
+
+for d in $(ls -d $1/asllib/tests/*.t/); do
+  for f in $(git ls-tree --name-only HEAD $d*.asl); do
+    if ! grep -q "${f##*/}" "${d}run.t"; then
+      echo "ASL file committed and not run: $f";
+      counter=$counter+1;
+    fi;
+  done
+done
+
+if [[ $counter -gt 0 ]]; then
+  exit 1;
+else
+  exit 0;
+fi;
