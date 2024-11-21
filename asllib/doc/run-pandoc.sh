2776c5024f268a8fb094131b009c7ec6f524ad27
crvdgc <ubikium@gmail.com>
2024-11-21 09:56:01 +0000

    fix: listing label extraction

--- asllib/doc/run-pandoc.sh
+++ asllib/doc/run-pandoc.sh
@@ -1,1 +1,1 @@
-pandoc --standalone --mathjax -o ASLReference.html ASLReference.tex
+LUA_PATH='filters/?.lua' pandoc --standalone --mathjax --filter ~/Downloads/pandoc-crossref -L filters/listing-label.lua -o ASLReference.html ASLReference.tex
