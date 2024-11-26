a2d95eab2745b62687be85e48449b07a0b419638
crvdgc <ubikium@gmail.com>
2024-11-26 10:47:46 +0000

    feat: hyperlink rewrite filter

--- asllib/doc/filters/aslref.lua
+++ asllib/doc/filters/aslref.lua
@@ -1,43 +1,79 @@
 local logging = require("logging")
 
-local function matchMacro(s, pattern)
+--- find first match of pattern and capture all groups according to arity
+--- @param s string
+--- @param pattern string
+--- @param arity integer
+local function matchMacro(s, pattern, arity)
   local i, j = string.find(s, pattern)
-  local groups = nil
-  if i then
-    groups = { string.match(string.sub(s, i, j), pattern) }
+  local groups = {}
+  local captured = 0
+  if i ~= nil then
+    -- j is '{' after "\name"
+    local l = j + 1
+    local stack = 1
+    for r = l, string.len(s) do
+      if captured >= arity or l >= #s then
+        break
+      end
+      local c = string.sub(s, r, r)
+      -- logging.temp("c", c)
+      -- logging.temp("stack", stack)
+      if c == "{" then
+        if stack == 0 then
+          l = r + 1
+        end
+        stack = stack + 1
+      elseif c == "}" then
+        stack = stack - 1
+        if stack == 0 then
+          -- logging.temp("found", r)
+          table.insert(groups, string.sub(s, l, r - 1))
+          captured = captured + 1
+          j = r
+        end
+      end
+    end
     -- logging.temp("groups", groups)
   end
   return i, j, groups
 end
 
 local function matchMacros(s, name, arity)
-  local pattern = "\\" .. name
-  for _ = 1, arity do
-    pattern = pattern .. "{(.*)}";
-  end
+  local pattern = "\\" .. name .. "{"
   -- logging.temp("pattern", pattern)
   local res = {}
   local rest = s
   local cur = 0
   while true do
+    -- logging.temp("rest", rest)
     if rest == "" then
       break
     end
-    local i, j, groups = matchMacro(rest, pattern)
+    local i, j, groups = matchMacro(rest, pattern, arity)
     if not i or not j then
       break
     end
+    -- logging.temp("i", i)
+    -- logging.temp("j", j)
+    -- logging.temp("groups", groups)
+    -- logging.temp("matched", string.sub(rest, i, j))
+    -- logging.temp("matched", string.sub(s, i + cur, j + cur))
+    -- logging.temp("cur", cur)
     table.insert(res, { i + cur, j + cur, groups })
-    cur = cur + j + 1
+    cur = cur + j
+    -- logging.temp("cur'", cur)
     rest = string.sub(rest, j + 1)
+    -- logging.temp("rest", rest)
   end
+  -- logging.temp("res", res)
   return res
 end
 
 --- Substitute a macro \name with arity by applying f to each matched argument
 --- @param s string
 --- @param name string
---- @param arity number
+--- @param arity integer
 --- @param f fun(groups: string[]): string
 --- @return string
 local function subMacros(s, name, arity, f)
