eb4b37d352fef436813ed2229fead9135d6b1da8
crvdgc <ubikium@gmail.com>
2024-11-26 13:07:11 +0000

    [asl,doc] Add links in math

--- asllib/doc/run-pandoc.sh
+++ asllib/doc/run-pandoc.sh
@@ -1,4 +1,5 @@
 LUA_PATH='filters/?.lua' pandoc --standalone --mathjax \
+  --include-in-header header.html \
   --css pandoc.css \
   --filter ~/Downloads/pandoc-crossref \
   -L filters/aslref.lua \
--- asllib/doc/run-test.sh
+++ asllib/doc/run-test.sh
@@ -1,1 +1,1 @@
-LUA_PATH='filters/?.lua' pandoc --standalone --mathjax --filter ~/Downloads/pandoc-crossref -L filters/aslref.lua -o test.html test.tex # && open test.html
+LUA_PATH='filters/?.lua' pandoc --standalone --mathjax -H header.html --filter ~/Downloads/pandoc-crossref -L filters/aslref.lua -o test.html test.tex # && open test.html
