4fd6e8b7996e58230fb3c2277f1e5cc78872ef3e
crvdgc <ubikium@gmail.com>
2024-11-26 10:47:46 +0000

    feat: hyperlink rewrite filter

--- asllib/doc/run-pandoc.sh
+++ asllib/doc/run-pandoc.sh
@@ -1,1 +1,6 @@
-LUA_PATH='filters/?.lua' pandoc --standalone --mathjax --filter ~/Downloads/pandoc-crossref -L filters/aslref.lua -o ASLReference.html ASLReference.tex
+LUA_PATH='filters/?.lua' pandoc --standalone --mathjax \
+  --css pandoc.css \
+  --filter ~/Downloads/pandoc-crossref \
+  -L filters/aslref.lua \
+  -o ASLReference.html \
+  ASLReference.tex
