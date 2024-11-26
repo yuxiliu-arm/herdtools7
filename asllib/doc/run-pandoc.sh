11213ab0ad8232d11de980fc4719b1a015ba110d
crvdgc <ubikium@gmail.com>
2024-11-26 13:31:56 +0000

    refactor: move to public

--- asllib/doc/run-pandoc.sh
+++ asllib/doc/run-pandoc.sh
@@ -1,6 +1,6 @@
 LUA_PATH='filters/?.lua' pandoc --standalone --mathjax \
-  --include-in-header header.html \
-  --css pandoc.css \
+  --include-in-header public/header.html \
+  --css public/pandoc.css \
   --filter ~/Downloads/pandoc-crossref \
   -L filters/aslref.lua \
   -o ASLReference.html \
