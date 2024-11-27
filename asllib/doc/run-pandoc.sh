d8d869e782e46eb047aef02c019cf873c55b80f6
crvdgc <ubikium@gmail.com>
2024-11-27 15:47:36 +0000

    feat: chunkedhtml

--- asllib/doc/run-pandoc.sh
+++ asllib/doc/run-pandoc.sh
@@ -1,11 +1,20 @@
+set -e
+
 PATH="/Users/yuxliu01/dev/tree-sitter-asl-highlight/target/release:$PATH" \
   LUA_PATH='filters/?.lua' \
   pandoc \
   --standalone \
+  --toc \
+  --split-level 1 \
   --mathjax \
   --include-in-header public/header.html \
   --css public/pandoc.css \
   --filter ~/Downloads/pandoc-crossref \
   -L filters/aslref.lua \
-  -o ASLReference.html \
+  -t chunkedhtml \
+  -o out \
   ASLReference.tex
+
+mkdir -p out/public
+
+cp public/pandoc.css out/public/pandoc.css
