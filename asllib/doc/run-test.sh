b21bb0cd6e0ec3443241bf26b41840a78e34b16f
crvdgc <ubikium@gmail.com>
2024-12-02 13:02:41 +0000

    { pairing test

--- asllib/doc/run-test.sh
+++ asllib/doc/run-test.sh
@@ -1,1 +1,1 @@
-PATH="/Users/yuxliu01/dev/tree-sitter-asl-highlight/target/release:$PATH" LUA_PATH='filters/?.lua' pandoc --standalone --mathjax -H public/header.html --filter ~/Downloads/pandoc-crossref -L filters/aslref.lua -o test test.tex -t chunkedhtml # && open test.html
+PATH="/Users/yuxliu01/dev/tree-sitter-asl-highlight/target/release:$PATH" LUA_PATH='filters/?.lua' pandoc --standalone --mathjax -H public/header.html --filter ~/Downloads/pandoc-crossref -L filters/aslref.lua -o test test.tex -o test.html
