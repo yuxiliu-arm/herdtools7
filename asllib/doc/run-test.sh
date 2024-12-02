b28b42ecf2ab803f2f795e56b0c5e9525cff7869
crvdgc <ubikium@gmail.com>
2024-12-02 13:41:44 +0000

    fix: remove ASTComment overrides

--- asllib/doc/run-test.sh
+++ asllib/doc/run-test.sh
@@ -1,1 +1,17 @@
-PATH="/Users/yuxliu01/dev/tree-sitter-asl-highlight/target/release:$PATH" LUA_PATH='filters/?.lua' pandoc --standalone --mathjax -H public/header.html --filter ~/Downloads/pandoc-crossref -L filters/aslref.lua -o test test.tex -o test.html
+if [ "$#" -eq 0 ]; then
+  out=( -o test.html )
+else
+  out=( "$@" )
+fi
+
+PATH="/Users/yuxliu01/dev/tree-sitter-asl-highlight/target/release:$PATH" \
+  LUA_PATH='filters/?.lua' \
+  pandoc \
+  --standalone \
+  --mathjax \
+  -H public/header.html \
+  --filter ~/Downloads/pandoc-crossref \
+  -L filters/aslref.lua \
+  --wrap preserve \
+  test.tex \
+  "${out[@]}"
