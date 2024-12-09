0d794cb04039bee6fe038d879f9fd4eadadce7d0
crvdgc <ubikium@gmail.com>
2024-12-09 10:01:16 +0000

    feat: add anchors to headings

--- asllib/doc/filters/aslref.lua
+++ asllib/doc/filters/aslref.lua
@@ -244,6 +244,22 @@ local function getMathId(path, idMaps, elem)
   end
 end
 
+-- https://github.com/jgm/pandoc-website/pull/50
+-- Adds anchor links to headings with IDs.
+function Header(h)
+  if h.identifier ~= '' then
+    -- an empty link to this header
+    local anchor_link = pandoc.Link(
+      {},                                            -- content
+      '#' .. h.identifier,                           -- href
+      '',                                            -- title
+      { class = 'anchor', ['aria-hidden'] = 'true' } -- attributes
+    )
+    h.content:insert(1, anchor_link)
+    return h
+  end
+end
+
 function Pandoc(doc)
   local idMaps = {}
   local chunks = pandoc.structure.split_into_chunks(doc, {
